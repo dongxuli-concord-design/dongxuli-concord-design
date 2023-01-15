b7852d08c6e47622c643eefe9e597476615f8f6f
Xinlin Cao <xinlin.cao@concord.design>
2023-01-15 15:58:11 +0800

    Refactor code

--- Apps/Automation/XcAtDocBlock.js
+++ Apps/Automation/XcAtDocBlock.js
@@ -1,11 +1,15 @@
 class XcAtDocBlock extends Xc3dDocDrawableObject {
-  x;
-  y;
-  z;
-  matrix;
-  color;
+  #x;
+  #y;
+  #z;
+  #color;
+  #transparent;
+  #opacity;
 
+  #body;
   #renderingObject;
+
+  matrix;
 
   constructor({
                 name = 'XcAtDocBlock',
@@ -14,24 +18,85 @@ class XcAtDocBlock extends Xc3dDocDrawableObject {
                 z,
                 coordinateSystem = new XcGmCoordinateSystem(),
                 color = new THREE.Color('gray'),
+                transparent = false,
+                opacity = 1.0,
               }) {
     super({name});
-    this.x = x;
-    this.y = y;
-    this.z = z;
+    this.#x = x;
+    this.#y = y;
+    this.#z = z;
     this.matrix = new XcGm3dMatrix();
-    this.color = color.clone();
+    this.#color = color.clone();
+    this.#transparent = transparent;
+    this.#opacity = opacity;
 
-    const body = XcGmBody.createSolidBlock({
+    this.#body = XcGmBody.createSolidBlock({
       x: this.x,
       y: this.y,
       z: this.z,
       coordinateSystem,
     });
+    this.#genereateBasicRenderingObject();
+  }
+
+  #genereateBasicRenderingObject() {
     this.#renderingObject = Xc3dDocDocument.generateRenderingForBody({
-      body,
-      color: this.color,
+      body: this.#body,
+      color: this.#color,
+      transparent: this.#transparent,
+      opacity: this.#opacity,
     });
+  }
+
+  get x() {
+    return this.#x;
+  }
+
+  set x(value) {
+    XcSysAssert({assertion: false, message: 'Cannot change size.'});
+  }
+
+  get y() {
+    return this.#y;
+  }
+
+  set y(value) {
+    XcSysAssert({assertion: false, message: 'Cannot change size.'});
+  }
+
+  get z() {
+    return this.#z;
+  }
+
+  set z(value) {
+    XcSysAssert({assertion: false, message: 'Cannot change size.'});
+  }
+
+  get color() {
+    return this.#color;
+  }
+
+  set color(value) {
+    this.#color = value;
+    this.#genereateBasicRenderingObject();
+  }
+
+  get transparent() {
+    return this.#transparent;
+  }
+
+  set transparent(value) {
+    this.#transparent = value;
+    this.#genereateBasicRenderingObject();
+  }
+
+  get opacity() {
+    return this.#opacity;
+  }
+
+  set opacity(value) {
+    this.#opacity = value;
+    this.#genereateBasicRenderingObject();
   }
 
   static load({json, document}) {
@@ -43,8 +108,10 @@ class XcAtDocBlock extends Xc3dDocDrawableObject {
     const matrix = XcGm3dMatrix.fromJSON({json: json.matrix});
     const color = new THREE.Color();
     color.fromArray(json.color);
+    const transparent = json.transparent;
+    const opacity = json.opacity;
 
-    const obj = new XcAtDocBlock({name, x, y, z, matrix, color});
+    const obj = new XcAtDocBlock({name, x, y, z, matrix, color, transparent, opacity});
 
     const userData = json.userData;
     obj.userData = userData;
@@ -54,12 +121,14 @@ class XcAtDocBlock extends Xc3dDocDrawableObject {
 
   clone() {
     const newObj = new XcAtDocBlock({
-      x: this.x,
-      y: this.y,
-      z: this.z,
+      x: this.#x,
+      y: this.#y,
+      z: this.#z,
       name: this.name,
       matrix: this.matrix.clone(),
-      color: this.color.clone(),
+      color: this.#color.clone(),
+      transparent: this.#transparent,
+      opacity: this.#opacity,
     });
 
     newObj.userData = {...this.userData};
@@ -74,21 +143,25 @@ class XcAtDocBlock extends Xc3dDocDrawableObject {
     this.z = other.z;
     this.matrix = other.matrix.clone();
     this.color = other.color.clone();
+    this.transparent = other.transparent;
+    this.opacity = other.opacity;
   }
 
   save({document}) {
     const data = super.save({document});
 
     const color = [];
-    this.color.toArray(color);
+    this.#color.toArray(color);
 
     return {
       ...data,
-      x: this.x,
-      y: this.y,
-      z: this.z,
+      x: this.#x,
+      y: this.#y,
+      z: this.#z,
       matrix: this.matrix.toJSON(),
       color,
+      transparent: this.#transparent,
+      opacity: this.#opacity,
     }
   }
 
