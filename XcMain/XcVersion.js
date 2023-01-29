b4d89e47f02cc0da0fbf81e71e5a17b5c1a9f688
Xinlin Cao <xinlin.cao@concord.design>
2023-01-29 10:29:01 +0800

    Upgrade Three.js and Bootstrap

--- XcMain/XcVersion.js
+++ XcMain/XcVersion.js
@@ -1,8 +1,8 @@
 let XcVersion = {
   "NW": "0.61.0 (Chromium 98 + Node 17.3.0)",
   "Parasolid": "28.1 (x64)",
-  "Three.js": "r137",
-  "Bootstrap": "v4.2.1",
+  "Three.js": "r149",
+  "Bootstrap": "5.3.0",
   "XcGm": "",
   "XcGmPk": "",
   "XcUI": "",
