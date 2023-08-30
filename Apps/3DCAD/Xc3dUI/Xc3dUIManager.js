87e50bc5d3584e5c427077b3560ca8283fc3d9e3
xcao21 <xinlincao@gmail.com>
2023-08-30 14:37:12 +0800

    Refactor

--- Apps/3DCAD/Xc3dUI/Xc3dUIManager.js
+++ Apps/3DCAD/Xc3dUI/Xc3dUIManager.js
@@ -124,10 +124,10 @@ class Xc3dUIManager {
 
     // Lights
     Xc3dUIManager.mainScene.add(new THREE.AmbientLight(0xffffff, 0.2));
-    Xc3dUIManager.overlayScene.add(new THREE.HemisphereLight(0xffffff, 0xffffff, 0.7));
+    Xc3dUIManager.mainScene.add(new THREE.HemisphereLight(0x404040, 0xffffff, 0.5));
 
     Xc3dUIManager.overlayScene.add(new THREE.AmbientLight(0xffffff, 0.2));
-    Xc3dUIManager.overlayScene.add(new THREE.HemisphereLight(0xffffff, 0xffffff, 0.7));
+    Xc3dUIManager.overlayScene.add(new THREE.HemisphereLight(0x404040, 0xffffff, 0.5));
 
     Xc3dUIManager.webGLRenderer = new THREE.WebGLRenderer({
       antialias: true,
@@ -147,7 +147,7 @@ class Xc3dUIManager {
     Xc3dUIManager.renderingCamera = new THREE.OrthographicCamera(-10, 10, 10, -10, 1, 2000);
 
     // Camera light
-    Xc3dUIManager.renderingCamera.add(new THREE.PointLight(0xffffff));
+    Xc3dUIManager.renderingCamera.add(new THREE.PointLight(0xffffff, 0.5));
     Xc3dUIManager.mainScene.add(Xc3dUIManager.renderingCamera);
 
     Xc3dUIManager.resetCamera();
